29474a8c93d9176fd995f0f7fdd07676eabf7ec4
hanseulhee <3021062@gmail.com>
2023-08-18 07:50:09 +0900

    add color theme

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -2,6 +2,13 @@ const theme = {
   color: {
     white: "#ffffff",
     black: "#000000",
+    warmBlack: "#252323",
+    blue: "#70798c",
+    coolGray: "#d9d9d9",
+    brown: "#a99985",
+    yellow: "#ffe380",
+    pink: "#e4bdc4",
+    warmGray: "#f5f1ed",
   },
   fontWeight: {
     light: 300,
