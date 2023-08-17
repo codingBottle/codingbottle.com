74121ca1e2abf2ce97ff773aae44cfd0903c2d60
hanseulhee <3021062@gmail.com>
2023-08-17 14:14:04 +0900

    mobile layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
+import { css } from "@emotion/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div css={layout}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
+
+const layout = css`
+  max-width: 480px;
+  min-height: calc(var(--var, 1vh) * 100);
+  width: 100%;
+  height: 100vh;
+  margin: 0 auto;
+`;
