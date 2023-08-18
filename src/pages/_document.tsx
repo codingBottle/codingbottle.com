3a0720203211a7ae9daed8693730e0f7108f4a06
hanseulhee <3021062@gmail.com>
2023-08-19 01:55:24 +0900

    add meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="kr">
-      <Head />
+      <Head>
+        <meta
+          name="keywords"
+          content="코딩보틀, 개발, 디자인, 성공회대, 동아리, 항해, 개발자"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="ko_KR" />
+      </Head>
       <body>
         <Main />
         <NextScript />
