1b5457b7a3c6fc9281bbf97a6ac8a54e7a6be3bf
hanseulhee <3021062@gmail.com>
2023-08-26 17:40:23 +0900

    modify inform

--- src/constants/core.ts
+++ src/constants/core.ts
@@ -1,5 +1,5 @@
 export const WEB_CORE = [
-  "수원 출신 온수냠냠냠 개발자",
+  "웹이 너무 좋은 사람",
   "더 멋진 개발자가 되기 위한 중국 출장",
 ];
 
@@ -8,9 +8,6 @@ export const SERVER_CORE = [
   "진격의 2학년",
 ];
 
-export const MOBILE_CORE = [
-  "박.신.영",
-  "Solution Challenge TOP 100",
-];
+export const MOBILE_CORE = ["박.신.영", "Solution Challenge TOP 100"];
 
 export const DESIGN_CORE = ["앱개발하다 디자인이 좋아진 사람"];
